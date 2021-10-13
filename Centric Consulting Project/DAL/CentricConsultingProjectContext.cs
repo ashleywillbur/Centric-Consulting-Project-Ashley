023e49d14485fa4808825ef5db99f69cd1d4aad7
Ashley Willbur <aw344317@ohio.edu>
2021-10-13 10:27:50 -0400

    changes

---
+++ Centric Consulting Project/DAL/CentricConsultingProjectContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Centric_Consulting_Project.Models;
+
+namespace Centric_Consulting_Project.DAL
+{
+    public class CentricConsultingProjectContext : DbContext 
+    {
+        public CentricConsultingProjectContext() : base("name=DefaultConnection") 
+        {
+        }
+            public DbSet<Profile> profiles { get; set; }
+
+    }
+}
--- Centric Consulting Project/Models/Profile.cs
+++ Centric Consulting Project/Models/Profile.cs
@@ -7,5 +7,16 @@ namespace Centric_Consulting_Project.Models
 {
     public class Profile
     {
+        public int EmployeeId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string businessUnit { get; set; }
+        // Boston, Charlotte, Chicago, Cincinnati, Cleveland, Columbus, Detroit, India, Indianapolis, 
+        // Louisville, Miami, Seattle, and St. Louis. 
+        public DateTime hireDate { get; set; }
+        public string title { get; set; }
+        // Consultant, Senior Consultant, Manager, Senior Manager, Architect, Senior Architect, Partner, and National.
+        public string email { get; set; }
+        public string phone { get; set; }
     }
 }
