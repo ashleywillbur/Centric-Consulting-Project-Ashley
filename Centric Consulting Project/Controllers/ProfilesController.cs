b8e024bdb5ae542249bc546dfcc7fdfe0abe36df
Ashley Willbur <aw344317@ohio.edu>
2021-10-20 10:39:46 -0400

    a

---
+++ Centric Consulting Project/Controllers/ProfilesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Centric_Consulting_Project.DAL;
+using Centric_Consulting_Project.Models;
+
+namespace Centric_Consulting_Project.Controllers
+{
+    public class ProfilesController : Controller
+    {
+        private CentricConsultingProjectContext db = new CentricConsultingProjectContext();
+
+        // GET: Profiles
+        public ActionResult Index()
+        {
+            return View(db.profiles.ToList());
+        }
+
+        // GET: Profiles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = db.profiles.Find(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // GET: Profiles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profiles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeId,firstName,lastName,BusinessUnit,hireDate,Title,email,Phone")] Profile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.profiles.Add(profile);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(profile);
+        }
+
+        // GET: Profiles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = db.profiles.Find(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // POST: Profiles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeId,firstName,lastName,BusinessUnit,hireDate,Title,email,Phone")] Profile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(profile).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(profile);
+        }
+
+        // GET: Profiles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = db.profiles.Find(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // POST: Profiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Profile profile = db.profiles.Find(id);
+            db.profiles.Remove(profile);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Centric Consulting Project/Models/Profile.cs
+++ Centric Consulting Project/Models/Profile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,16 +8,19 @@ namespace Centric_Consulting_Project.Models
 {
     public class Profile
     {
+        [Key]
         public int EmployeeId { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
-        public string businessUnit { get; set; }
+        public enum BusinessUnitName { Boston, Charlotte, Chicago, Cincinnati, Cleveland, Columbus, Detroit, India, Indianapolis, Louisville, Miami, Seattle, StLouis }
+        public BusinessUnitName BusinessUnit { get; set; }
         // Boston, Charlotte, Chicago, Cincinnati, Cleveland, Columbus, Detroit, India, Indianapolis, 
         // Louisville, Miami, Seattle, and St. Louis. 
         public DateTime hireDate { get; set; }
-        public string title { get; set; }
+        public enum BusinessTitle { Consultant, SeniorConsultant, Manager, SeniorManager, Architect, SeniorArchitect, Partner, National }
+        public BusinessTitle Title{ get; set; }
         // Consultant, Senior Consultant, Manager, Senior Manager, Architect, Senior Architect, Partner, and National.
         public string email { get; set; }
-        public string phone { get; set; }
+        public string Phone { get; set; }
     }
 }
